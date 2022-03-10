0320329244789fee1e5fdfdd21c656f81be6ef35
Jack Grimes <flangojack@hotmail.com>
2022-03-10 12:51:17 +0000

    Adding implementation of Club class.

---
+++ src/main/java/com/kapflagon/teamlodge/model/Club.java
@@ -0,0 +1,33 @@
+package com.kapflagon.teamlodge.model;
+
+public class Club {
+
+
+    // Variables
+    private String name;
+
+
+    // Constructors
+    public Club(String name) {
+        this.name = name;
+    }
+
+
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    // Initialisation methods
+
+
+    // Other methods
+
+
+}
