c66ba39af0da4e8e0c2757fbf85e95b877df24b6
Jack Grimes <flangojack@hotmail.com>
2022-03-11 22:09:19 +0000

    Creating a virtually empty class for a ClubController.

---
+++ src/main/java/com/kapflagon/teamlodge/controllers/ClubController.java
@@ -0,0 +1,27 @@
+package com.kapflagon.teamlodge.controllers;
+
+import com.kapflagon.teamlodge.persistence.repositories.ClubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClubController {
+
+
+    // Variables
+    @Autowired
+    private final ClubRepository clubRepository;
+
+    // Constructors
+
+
+    // Getters and Setters
+
+
+    // Initialisation methods
+
+
+    // Other methods
+
+
+}
