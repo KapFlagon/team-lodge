3b4751343779e9e715d5a00073adcb90a1eb1766
Jack Grimes <flangojack@hotmail.com>
2022-03-11 22:08:01 +0000

    Adding JPA annotations to the Club class for persistence.

--- src/main/java/com/kapflagon/teamlodge/model/Club.java
+++ src/main/java/com/kapflagon/teamlodge/model/Club.java
@@ -1,17 +1,32 @@
 package com.kapflagon.teamlodge.model;
 
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
 public class Club {
 
 
     // Variables
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID id;
     private String name;
 
 
     // Constructors
+    protected Club() {
+        // Must be empty for JPA annotations.
+    }
+
     public Club(String name) {
         this.name = name;
     }
-
 
 
     // Getters and Setters
